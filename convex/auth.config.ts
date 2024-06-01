a5affa434291bf2755d08aaeb7451ebdbadd9401
samj10022004 <97349711+samj10022004@users.noreply.github.com>
2024-06-02 02:57:00 +0530

    first initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://kind-ringtail-86.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
